834c535b47de6e664fd83b95b675944f23c2abc1
boyczuk <adlaibridsonboyczuk@gmail.com>
2024-01-05 19:42:49 -0500

    Basic site up, added navbar

--- src/components/FloatingNavbar.tsx
+++ src/components/FloatingNavbar.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './FloatingNavbar.css';
 
-function Navbar() {
+interface NavbarProps {
+    className?: string;
+}
+
+function Navbar(props: NavbarProps) {
+    
+    const [activeSection, setActiveSection] = useState('');
+
+    const scrollToSection = (sectionId: string) => {
+        const section = document.getElementById(sectionId);
+        if (section) { 
+            section.scrollIntoView({ behavior: 'smooth' });
+            setActiveSection(sectionId);
+        }
+    }
+
+    // Add logic for scrolling based on where the user is looking rather than navbar
+
     return (
-        <div className="navbar-box">
-            <p className="navbar-button">About</p>
-            <p className="navbar-button">Skills</p>
-            <p className="navbar-button">Courses</p>
-            <p className="navbar-button">Projects</p>
+        <div className={`navbar-box ${props.className}`}>
+            <p className={`navbar-button ${activeSection === 'about' ? 'active' : ''}`} onClick={() => scrollToSection('about')}>About</p>
+            <p className={`navbar-button ${activeSection === 'skills' ? 'active' : ''}`} onClick={() => scrollToSection('skills')}>Skills</p>
+            <p className={`navbar-button ${activeSection === 'courses' ? 'active' : ''}`} onClick={() => scrollToSection('courses')}>Courses</p>
+            <p className={`navbar-button ${activeSection === 'projects' ? 'active' : ''}`} onClick={() => scrollToSection('projects')}>Projects</p>
         </div>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,15 @@
-import Navbar from '../components/FloatingNavbar';
-import Header from '../components/Header';
 import './Home.css';
+import resume from '../assets/resume.pdf';
 
 function HomePage() {
     return (
         <div className='container'>
-
-            <Navbar />
-
             <div className='content'>
-
                 <div className='sections'>
 
-                    <div className='about'>
+                    <div id="about" className='about'>
                         <h3>About me</h3>
-                        <p>Hi, my name is Adlai Bridson-Boyczuk I am a software developer currently in my 3rd year of studying Computer Science at Queen's University.
+                        <p>Hi, my name is Adlai I am a software developer currently in my 3rd year of studying Computer Science at Queen's University.
                             At the moment I am working at Yasu Energy as a Junior Web Developer.</p>
 
                         <p>During my time at Queen's University I have spent time developing projects in several organizations like QUANTT
@@ -25,30 +20,106 @@ function HomePage() {
                         <p>Contact me at 'adlaibridsonboyczuk@gmail.com' or any of the social media above!</p>
 
                         <p>My resume:</p>
-                        <a href="#">Resume</a>
+                        <a href={resume} target="_blank" style={{ color: "blue" }}>
+                            Resume
+                        </a>
                     </div>
 
-                    <div className='skills'>
-                        <h3>Skills</h3>
-                        <p>Languages: Python, JavaScript, Typescript, Java, C, Bash, x86 Assembly,</p>
-                        <p>Frameworks: HTML/CSS, jQuery, Node.js, Express.js, React, Flask, Unity</p>
-                        <p>Technologies: MongoDB, MySQL, pandas, NumPy, Pytest, Linux, Excel, Git, Docker, VS Code, NetBeans, Eclipse</p>
+                    <div id="skills" className='skills'>
+                        <h2>Technical Skills</h2>
+                        <li>
+                            <b>Languages:</b>
+                            <br></br>Python, JavaScript, Typescript, Java, C, Bash, x86 Assembly,
+                        </li>
+                        <li>
+                            <b>Frameworks:</b>
+                            <br></br>HTML/CSS, jQuery, Node.js, Express.js, React, Flask, Unity
+                        </li>
+                        <li>
+                            {" "}
+                            <b>Technologies:</b>
+                            <br></br>MongoDB, MySQL, pandas, NumPy, Pytest, Linux, Excel, Git,
+                            Docker, VS Code, NetBeans, Eclipse{" "}
+                        </li>
                     </div>
 
-                    <div className='courses'>
-                        <h3>Courses</h3>
-                        <p>Algorithms
-                            Data Structures
-                            Artificial Intelligence
-                            Neural & Genetic Computing
-                            Operating Systems
-                            Software Quality Assurance
-                            Software Architecture
-                            Functional Programming</p>
+                    <div id="courses" className='courses'>
+                        <h2>Relevant Courses</h2>
+                        <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-365" className="clickable-link" target="_blank">Algorithms</a></li>
+                        <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-235" className="clickable-link" target="_blank">Data Structures</a></li>
+                        <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-352" className="clickable-link" target="_blank">Artificial Intelligence</a></li>
+                        <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-452" className="clickable-link" target="_blank">Neural & Genetic Computing</a></li>
+                        <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-324" className="clickable-link" target="_blank">Operating Systems</a></li>
+                        <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-327" className="clickable-link" target="_blank">Software Quality Assurance</a></li>
+                        <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-322" className="clickable-link" target="_blank">Software Architecture</a></li>
+                        <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-360" className="clickable-link" target="_blank">Functional Programming</a></li>
                     </div>
 
-                    <div className='project'>
-                        <h3>Projects</h3>
+                    <div id="projects" className='project'>
+                        <p className="project-boxes">
+                            <h2>Projects</h2>
+                            <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">Equities Trading Algorithm</a></b> |{" "}
+                            <i>Python, Jupyter Notebooks, NumPy, pandas, QuantConnect, Scikit-learn</i>
+                            <li>
+                                Developed a Python securities trading algorithm that achieved 18%
+                                profit in 3-year backtest against S&P 500
+                            </li>
+                            <li>
+                                Implemented a SMA paired with a RSI indicator to determine when to
+                                short Technology and Real Estate securities.
+                            </li>
+                        </p>
+
+                        <b><a href="https://theworldofyasu.com" className="clickable-link" target="_blank">The World of Yasu</a></b> |{" "}
+                            <i>Typescript, React.js, Firebase, HTML, CSS, MaterialUI</i>
+                            <li>
+                                Dynamic marketing and sales website for an Energy drink company.
+                            </li>
+
+                        <p className="project-boxes">
+                            <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">Spending Tracker</a></b> |{" "}
+                            <i>Javascript, React, Flask, Electron, Sqlite3, HTML, CSS, MaterialUI</i>
+                            <li>
+                                Desktop app used to track, view, and better manage my personal finances.
+                            </li>
+                            <li>
+                                React frontend running on Electron, using Flask as a backend to store and organize expenses.
+                            </li>
+                        </p>
+
+                        <p className="project-boxes">
+                            <b><a href="https://github.com/mad-cat-lon/qbnb" className="clickable-link" target="_blank">QBNB</a></b> | <i>Python, Flask, MongoDB, Pytest, Linux, Docker</i>
+                            <li>
+                                Collaborated on a Python-based CLI app for property rentals and
+                                booking using a Flask server, managing user data in MongoDB
+                            </li>
+                            <li>
+                                Followed an agile development cycle and utilized Pytest for unit and
+                                integration testing to ensure product quality
+                            </li>
+                        </p>
+                        <p className="project-boxes">
+                            <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">UFC myGym</a></b> | <i>JavaScript, Node.js, Express.js, MongoDB, React</i>
+                            <li>
+                                MERN web application to track and train your favourite UFC fighters.
+                            </li>
+                        </p>
+
+                        <p className="project-boxes">
+                            <b><a href="https://github.com/boyczuk/myLibrary" className="clickable-link" target="_blank">myLibrary book tracker</a></b> | <i>C++, MySQL, Qt</i>
+                            <li>
+                                Designed and programmed a personal book manager to track and manage
+                                my reading habits
+                            </li>
+                        </p>
+
+                        <p className="project-boxes">
+                            <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">Forest Rumble</a></b> | <i>C#, Unity, HTML, Photoshop</i>
+                            <li>
+                                Led development of a 2D level-based platformer. Added novel physics
+                                mechanics to pair with player movement
+                            </li>
+                        </p>
                     </div>
                 </div>
             </div>
