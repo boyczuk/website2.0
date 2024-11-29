9a446b1f74947dee698013400453071c84d98782
adlai <adlaibridsonboyczuk@gmail.com>
2024-11-29 17:28:09 -0500

    noreferrer warning fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import './Home.css';
 import resume from '../assets/websiteresume.pdf';
 import me from '../assets/me.jpg';
 import me2 from '../assets/me2.jpg';
-import me3 from '../assets/me3.jpg';
 import me5 from '../assets/me5.jpg';
 import { FaLinkedin, FaGithub, FaFistRaised } from 'react-icons/fa';
 import FallingLeaves from '../components/FallingLeaves';
@@ -47,7 +46,7 @@ function HomePage() {
 
                             <p>Please contact me at <a className="clickable-link" href="mailto:adlaibridsonboyczuk@gmail.com">adlaibb@gmail.com</a></p>
                             <p>Thanks!</p>
-                            <a href={resume} target="_blank" className="rlink">
+                            <a href={resume} target="_blank" rel="noreferrer" className="rlink">
                                 Resume
                             </a>
                         </div>
@@ -71,14 +70,14 @@ function HomePage() {
 
                             <div id="courses" className='courses'>
                                 <h3>Relevant Courses</h3>
-                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-455" className="clickable-link" target="_blank">Evolutionary Optimization</a></li>
-                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-474" className="clickable-link" target="_blank">Reinforcement Learning</a></li>
-                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-365" className="clickable-link" target="_blank">Algorithms</a></li>
-                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-235" className="clickable-link" target="_blank">Data Structures</a></li>
-                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-352" className="clickable-link" target="_blank">Artificial Intelligence</a></li>
-                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-452" className="clickable-link" target="_blank">Neural & Genetic Computing</a></li>
-                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-324" className="clickable-link" target="_blank">Operating Systems</a></li>
-                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-327" className="clickable-link" target="_blank">Software Quality Assurance</a></li>
+                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-455" className="clickable-link" target="_blank" rel="noreferrer">Evolutionary Optimization</a></li>
+                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-474" className="clickable-link" target="_blank" rel="noreferrer">Reinforcement Learning</a></li>
+                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-365" className="clickable-link" target="_blank" rel="noreferrer">Algorithms</a></li>
+                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-235" className="clickable-link" target="_blank" rel="noreferrer">Data Structures</a></li>
+                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-352" className="clickable-link" target="_blank" rel="noreferrer">Artificial Intelligence</a></li>
+                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-452" className="clickable-link" target="_blank" rel="noreferrer">Neural & Genetic Computing</a></li>
+                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-324" className="clickable-link" target="_blank" rel="noreferrer">Operating Systems</a></li>
+                                <li><a href="https://www.cs.queensu.ca/undergraduate/courses/CISC-327" className="clickable-link" target="_blank" rel="noreferrer">Software Quality Assurance</a></li>
                             </div>
                         </div>
 
@@ -86,7 +85,7 @@ function HomePage() {
 
                             <p className="project-boxes">
                                 <h3>Projects</h3>
-                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">
+                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank" rel="noreferrer">
                                     UAV Detection Capstone Thesis</a></b> | <i>Python, PyTorch, YOLOv5, OpenCV, MATLAB, NumPy, pandas</i>
                                 <li>
                                     Developed a YOLOv5-based detection system that achieved 77% accuracy in distinguishing UAVs from other aerial objects. Utilized OpenCV for visualization and label data normalization, resulting in 94.4% precision and 93.2% recall.
@@ -96,14 +95,14 @@ function HomePage() {
 
 
                             <p className="project-boxes">
-                                <b><a href="https://github.com/boyczuk/web-arcade-rate" className="clickable-link" target="_blank">Arcade-Rate</a></b> | <i>TypeScript, Node.js, Express.js, Firebase, React</i>
+                                <b><a href="https://github.com/boyczuk/web-arcade-rate" className="clickable-link" target="_blank" rel="noreferrer">Arcade-Rate</a></b> | <i>TypeScript, Node.js, Express.js, Firebase, React</i>
                                 <li>
                                     Developed a full-stack web application that ranks, compares, and tracks video games. Integrated data from IGDB via an Express.js backend and hosted it with Firebase cloud functions.
                                 </li>
                             </p>
 
                             <p className="project-boxes">
-                                <b><a href="https://github.com/boyczuk/BlackMarket2D" className="clickable-link" target="_blank">BlackMarket2D</a></b> | <i>C#, Unity, 2D Game Development</i>
+                                <b><a href="https://github.com/boyczuk/BlackMarket2D" className="clickable-link" target="_blank" rel="noreferrer">BlackMarket2D</a></b> | <i>C#, Unity, 2D Game Development</i>
                                 <li>
                                     Developed a crime-oriented 2D isometric game where players manage autonomous NPCs and control a criminal organization, featuring base-building mechanics and dynamic interactions.
                                 </li>
@@ -111,14 +110,14 @@ function HomePage() {
 
 
                             <p className="project-boxes">
-                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">CNN for Tomato Disease Classification</a></b> | <i>Python, Jupyter Notebooks, TensorFlow, Keras</i>
+                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank" rel="noreferrer">CNN for Tomato Disease Classification</a></b> | <i>Python, Jupyter Notebooks, TensorFlow, Keras</i>
                                 <li>
                                     Built a Convolutional Neural Network that classified tomato leaf diseases into 10 classes using over 11,000 images, achieving a 75% validation accuracy by implementing dropout layers to combat overfitting.
                                 </li>
                             </p>
 
                             <p className="project-boxes">
-                                <b><a href="https://theworldofyasu.com" className="clickable-link" target="_blank">The World of Yasu</a></b> |{" "}
+                                <b><a href="https://theworldofyasu.com" className="clickable-link" target="_blank" rel="noreferrer">The World of Yasu</a></b> |{" "}
                                 <i>Typescript, React.js, Firebase, HTML, CSS, MaterialUI</i>
                                 <li>
                                     Created a dynamic marketing and sales webapp for an energy drink company with a custom animated intro.
@@ -126,7 +125,7 @@ function HomePage() {
                             </p>
 
                             <p className="project-boxes">
-                                <b><a href="https://proximacommand.com/" className="clickable-link" target="_blank">
+                                <b><a href="https://proximacommand.com/" className="clickable-link" target="_blank" rel="noreferrer">
                                     Proxima Command Starship Simulator</a></b> | <i>Python, Pygame, CircuitPython</i>
                                 <li>
                                     Programmed a Python-based starship emulator using Pygame, creating a proof-of-concept for a physical game
@@ -135,7 +134,7 @@ function HomePage() {
                             </p>
 
                             <p className="project-boxes">
-                                <b><a href="https://github.com/boyczuk/FlappyBird-ReinforcementLearning" className="clickable-link" target="_blank">
+                                <b><a href="https://github.com/boyczuk/FlappyBird-ReinforcementLearning" className="clickable-link" target="_blank" rel="noreferrer">
                                     Flappy Bird Q-Learning Reinforcement Learning Approach</a></b> | <i>Python, OpenAI Gym, Reinforcement Learning, NumPy, Pandas</i>
                                 <li>
                                     Implemented a model-free reinforcement learning algorithm to maximize the agent's score in the Flappy Bird game.
@@ -143,7 +142,7 @@ function HomePage() {
                             </p>
 
                             <p className="project-boxes">
-                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">Equities Trading Algorithm</a></b> |{" "}
+                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank" rel="noreferrer">Equities Trading Algorithm</a></b> |{" "}
                                 <i>Python, Jupyter Notebooks, NumPy, pandas, QuantConnect, Scikit-learn</i>
                                 <li>
                                     Created a Python-based trading algorithm that achieved an 18% profit over a 3-year backtest against the S&P 500. Combined SMA and RSI indicators to identify optimal short-selling opportunities in Tech and Real Estate sectors.
@@ -151,7 +150,7 @@ function HomePage() {
                             </p>
 
                             <p className="project-boxes">
-                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">Spending Tracker</a></b> |{" "}
+                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank" rel="noreferrer">Spending Tracker</a></b> |{" "}
                                 <i>Javascript, React, Flask, Electron, Sqlite3, HTML, CSS, MaterialUI</i>
                                 <li>
                                     Built a desktop application to track, view, and manage personal finances. Implemented a React frontend using Electron and a Flask backend for expense organization.
@@ -159,7 +158,7 @@ function HomePage() {
                             </p>
 
                             <p className="project-boxes">
-                                <b><a href="https://github.com/mad-cat-lon/qbnb" className="clickable-link" target="_blank">QBNB</a></b> | <i>Python, Flask, MongoDB, Pytest, Linux, Docker</i>
+                                <b><a href="https://github.com/mad-cat-lon/qbnb" className="clickable-link" target="_blank" rel="noreferrer">QBNB</a></b> | <i>Python, Flask, MongoDB, Pytest, Linux, Docker</i>
                                 <li>
                                     Collaborated on a Python-based property rental and booking CLI app, using Flask to manage user data in MongoDB.
                                 </li>
@@ -169,7 +168,7 @@ function HomePage() {
                             </p>
 
                             <p className="project-boxes">
-                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank">Forest Rumble</a></b> | <i>C#, Unity, HTML, Photoshop</i>
+                                <b><a href="https://github.com/boyczuk" className="clickable-link" target="_blank" rel="noreferrer">Forest Rumble</a></b> | <i>C#, Unity, HTML, Photoshop</i>
                                 <li>
                                     Led the development of a 2D level-based platformer, introducing novel physics mechanics to enhance player movement.
                                 </li>
